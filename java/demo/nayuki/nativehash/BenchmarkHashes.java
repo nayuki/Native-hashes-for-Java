07e705c782a6cf3b53efb915d0a61765df64989e
Nayuki Minase <nayuki@eigenstate.org>
2014-11-09 03:37:26 +0000

    Added demo main programs "sha1sum" and "BenchmarkHashes".

---
+++ java/demo/nayuki/nativehash/BenchmarkHashes.java
@@ -0,0 +1,83 @@
+package nayuki.nativehash;
+
+import java.util.Random;
+
+
+public class BenchmarkHashes {
+	
+	public static void main(String[] args) {
+		BlockHasher[] hashers;
+		if (args[0].equals("java")) {
+			hashers = new BlockHasher[] {
+				new Md2Java(),
+				new Md4Java(),
+				new Md5Java(),
+				new Ripemd128Java(),
+				new Ripemd160Java(),
+				new Ripemd256Java(),
+				new Ripemd320Java(),
+				new Sha1Java(),
+				new Sha224Java(),
+				new Sha256Java(),
+				new Sha384Java(),
+				new Sha512Java(),
+				new TigerJava(),
+				new Tiger2Java(),
+				new WhirlpoolJava(),
+			};
+		} else if (args[0].equals("native")) {
+			hashers = new BlockHasher[] {
+				new Md2(),
+				new Md4(),
+				new Md5(),
+				new Ripemd128(),
+				new Ripemd160(),
+				new Ripemd256(),
+				new Ripemd320(),
+				new Sha1(),
+				new Sha224(),
+				new Sha256(),
+				new Sha384(),
+				new Sha512(),
+				new Tiger(),
+				new Tiger2(),
+				new Whirlpool(),
+			};
+		} else
+			throw new IllegalArgumentException();
+		
+		int trials = 1;
+		if (args.length == 2)
+			trials = Integer.parseInt(args[1]);
+		
+		byte[] buffer = new byte[64 * 1024 * 1024];
+		new Random().nextBytes(buffer);
+		for (BlockHasher hasher : hashers) {
+			long len = 1000;
+			while (getTime(hasher, buffer, len) < 1000000000)  // Warm up the JIT
+				len *= 4;
+			
+			long time = getTime(hasher, buffer, len);
+			for (int i = 0; i < trials; i++) {
+				len = Math.round(len * 1e9 / time);  // Try to target 1.0 second
+				time = getTime(hasher, buffer, len);
+				System.out.printf("%-35s  %12d B  %12d ns  %8.2f MiB/s%n", hasher.getClass().getName(), len, time, len * 1e9 / time / 1048576);
+			}
+			if (trials > 1)
+				System.out.println();
+		}
+	}
+	
+	
+	// Returns nanoseconds
+	private static long getTime(BlockHasher hasher, byte[] buffer, long len) {
+		long startTime = System.nanoTime();
+		while (len > 0) {
+			int n = (int)Math.min(len, buffer.length);
+			hasher.update(buffer, 0, n);
+			len -= n;
+		}
+		return System.nanoTime() - startTime;
+	}
+	
+}
---
+++ java/demo/sha1sum.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import nayuki.nativehash.Sha1;
+
+
+public class sha1sum {
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length == 0) {
+			System.err.println("Usage: java sha1sum FILES...");
+			System.exit(1);
+		}
+		
+		int status = 0;
+		for (String arg : args) {
+			InputStream in = null;
+			try {
+				if (arg.equals("-"))
+					in = System.in;
+				else {
+					File file = new File(arg);
+					if (file.isDirectory()) {
+						System.err.println("sha1sum: " + arg + ": Is a directory");
+						status = 1;
+						continue;
+					} else if (!file.exists()) {
+						System.err.println("sha1sum: " + arg + ": No such file or directory");
+						status = 1;
+						continue;
+					} else {
+						in = new FileInputStream(file);
+					}
+				}
+				
+				Sha1 hasher = new Sha1();
+				byte[] buf = new byte[64 * 1024];
+				while (true) {
+					int n = in.read(buf);
+					if (n == -1)
+						break;
+					hasher.update(buf, 0, n);
+				}
+				
+				byte[] hash = hasher.getHash();
+				StringBuilder sb = new StringBuilder();
+				for (byte b : hash) {
+					sb.append(Integer.toString((b >>> 4) & 0xF, 16))
+					  .append(Integer.toString((b >>> 0) & 0xF, 16));
+				}
+				System.out.println(sb.toString() + "  " + arg);
+				
+			} finally {
+				if (in != null && in != System.in)
+					in.close();
+			}
+		}
+		System.exit(status);
+	}
+	
+}
